eb3122756507392aa80d4f56505f7442dd716153
psychegrammer <psychegrammer@gmail.com>
2019-07-26 13:09:06 +0200

    Added 2 layouts (recipe list & list row)

---
+++ app/src/main/java/psychegrammer/example/recipefinderapp/RecipeList.kt
@@ -0,0 +1,12 @@
+package psychegrammer.example.recipefinderapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class RecipeList : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recipe_list)
+    }
+}
